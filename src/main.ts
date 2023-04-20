40295c5717348687a372c6672f26f9a7cab305e2
Sooniii <theodulejanchen@hotmail.fr>
2023-04-20 15:54:03 +0200

    start chess game

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,51 @@
 import '../node_modules/cm-chessboard-ts/assets/styles/cm-chessboard.css'
-import {Chessboard} from '../node_modules/cm-chessboard-ts/src/cm-chessboard/Chessboard.js'
+import {Chessboard, COLOR, INPUT_EVENT_TYPE} from 'cm-chessboard-ts'
+import {Chess} from 'chess.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <div id="chessContainer"></div>
 `
+const chess = new Chess
+const board = new Chessboard(document.getElementById("chessContainer") !, {
+  position: 'start',
+  sprite: {
+    url: 'images/chessboard-sprite.svg',
+    size : 40,
+    cache : true
+  },
+  }
+)
 
-let chessContainer = document.getElementById("chessContainer")
+board.enableMoveInput((event) => {
+  switch (event.type) {
+    case INPUT_EVENT_TYPE.moveInputStarted:
+      console.log(`moveInputStarted: ${event.square}`)
+      return true
+    case INPUT_EVENT_TYPE.validateMoveInput:
+      chess.move()
+      console.log(`validateMoveInput: ${event.squareFrom}-${event.squareTo}`)
+      return true
+    case INPUT_EVENT_TYPE.moveInputCanceled:
+      console.log(`moveInputCanceled`)
+  }
+}, COLOR.white)
 
-if (chessContainer) {
-  new Chessboard(chessContainer), {position: "rn2k1r1/ppp1pp1p/3p2p1/5bn1/P7/2N2B2/1PPPPP2/2BNK1RR"}
+board.enableMoveInput((event) => {
+  switch (event.type) {
+    case INPUT_EVENT_TYPE.moveInputStarted:
+      console.log(`moveInputStarted: ${event.square}`)
+      return true
+    case INPUT_EVENT_TYPE.validateMoveInput:
+      console.log(`validateMoveInput: ${event.squareFrom}-${event.squareTo}`)
+      return true
+    case INPUT_EVENT_TYPE.moveInputCanceled:
+      console.log(`moveInputCanceled`)
+  }
+}, COLOR.black)
+
+/*while (!chess.gameOver()) {
+  const moves = chess.moves()
+  const move = moves[Math.floor(Math.random() * moves.length)]
+  chess.move(move)
 }
+console.log(chess.pgn())*/
